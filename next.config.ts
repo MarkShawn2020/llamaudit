8f1c393853edb042fa107d0c22e2f0652ab027d8
markshawn2020 <shawninjuly@gmail.com>
2025-03-15 23:24:33 +0800

    fix build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   experimental: {
     ppr: true
+  },
+  webpack: (config, { isServer }) => {
+    config.ignoreWarnings = [
+      { module: /node_modules\/any-promise/ }
+    ];
+    return config;
   }
 };
 
