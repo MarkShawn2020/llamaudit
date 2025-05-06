2ab25a473f0dd0f65009298cceb1a4d2b7bb5f53
markshawn2020 <shawninjuly@gmail.com>
2025-05-06 20:08:25 +0800

    fix signup

--- app/(login)/actions.ts
+++ app/(login)/actions.ts
@@ -234,7 +234,7 @@ export const signUp = validatedAction(signUpSchema, async (data, formData) => {
     return createCheckoutSession({ team: createdTeam, priceId });
   }
 
-  redirect('/dashboard');
+  redirect('/projects');
 });
 
 export async function signOut() {
