0d04d11f8d96278e7aa7aae3c014c56292c8de4e
markshawn2020 <shawninjuly@gmail.com>
2025-05-10 01:30:07 +0800

    fix navbar

--- components/GlobalNavbar.tsx
+++ components/GlobalNavbar.tsx
@@ -113,7 +113,7 @@ export function GlobalNavbar() {
 
 
   return (
-    <header className="sticky top-0 z-50 border-b border-gray-200  max-w-7xl mx-auto w-full">
+    <header className="sticky top-0 z-50 bg-white/95 backdrop-blur-sm  max-w-7xl mx-auto w-full border-0 border-b border-gray-200">
       <div className="container flex h-16 items-center justify-between px-4 sm:px-6 lg:px-8">
         <div className="flex items-center">
           <Link href="/" className="flex items-center gap-2 mr-6">
