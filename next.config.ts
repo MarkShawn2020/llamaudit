88780bfd83aee0591a6c33a710dfd8e437836244
markshawn2020 <shawninjuly@gmail.com>
2025-06-30 07:58:39 +0800

    ignore build error

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,9 @@ const nextConfig: NextConfig = {
     config.ignoreWarnings = [
       { module: /node_modules\/any-promise/ }
     ];
+    
+    // Ignore broken symlinks, ref: https://claude.ai/chat/2d76366f-bd90-4c85-9bfc-7d8d21e61721
+    config.resolve.symlinks = false;
     return config;
   }
 };
