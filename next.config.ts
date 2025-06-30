1eab4bc177d08a3b8877f73e27759a391eb5e5bd
markshawn2020 <shawninjuly@gmail.com>
2025-06-30 08:08:58 +0800

    rm links for build

--- next.config.ts
+++ next.config.ts
@@ -13,8 +13,6 @@ const nextConfig: NextConfig = {
       { module: /node_modules\/any-promise/ }
     ];
     
-    // Ignore broken symlinks, ref: https://claude.ai/chat/2d76366f-bd90-4c85-9bfc-7d8d21e61721
-    config.resolve.symlinks = false;
     return config;
   }
 };
